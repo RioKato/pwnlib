ceaf7cc42ab8ed77a0583eb009b18d61b9f5eb23
rio <rio@kato.com>
2024-10-16 02:13:53 +0900

    update

--- plugin/docker.py
+++ plugin/docker.py
@@ -18,12 +18,17 @@ class Docker(Outer):
         command += inner
         return command
 
+    def __sh(self, inner: list[str]) -> list[str]:
+        command = [self.sh, '-c']
+        command += ' '.join(inner)
+        return command
+
     def run(self, env: dict[str, str], aslr: bool) -> list[str]:
         from shlex import join
 
         command = self.command.run(env, aslr)
         command = join(command)
-        command = [self.sh, '-c', f'echo $$ > {self.pidfile}; exec {command};']
+        command = self.__sh([f'echo $$ > {self.pidfile}; exec {command};'])
         return self.__exec(command, '-i')
 
     def debug(self, env: dict[str, str], aslr: bool) -> list[str]:
@@ -33,8 +38,7 @@ class Docker(Outer):
     def attach(self, pid: int) -> list[str]:
         pid = f'$(cat {self.pidfile})'  # type:ignore
         command = self.command.attach(pid)
-        command = ' '.join(command)
-        command = [self.sh, '-c', command]
+        command = self.__sh(command)
         return self.__exec(command)
 
     def record(self, env: dict[str, str], aslr: bool) -> list[str]:
