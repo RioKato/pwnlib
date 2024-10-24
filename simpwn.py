c61ff5602d1027d10350d605a1a3fb96cd3dc607
rio <rio@kato.com>
2024-10-24 11:13:35 +0900

    update

--- simpwn.py
+++ simpwn.py
@@ -191,7 +191,7 @@ class Run(Target):
 
 
 @dataclass
-class GdbOpener:
+class __GdbOpener:
     command: Target
     host: str = ''
     port: int = 1234
@@ -219,44 +219,44 @@ class GdbOpener:
         return command
 
 
+class Gdb:
+    @dataclass
+    class Debugger(__GdbOpener, Debugger):
+        options: list[str] = field(default_factory=list)
+        gdbserver: str = 'gdbserver'
+        env: str = 'env'
+
+        def debug(self, *, env: dict[str, str] = {}, aslr: bool = True) -> list[str]:
+            command = [self.gdbserver]
+
+            if aslr:
+                command += ['--no-disable-randomization']
+            else:
+                command += ['--disable-randomization']
+
+            if env:
+                command += ['--wrapper', self.env]
+
+                for k, v in env.items():
+                    command += [f'{k}={v}']
+
+                command += ['--']
+
+            command += self.options
+            command += [f'{self.host}:{self.port}']
+            command += self.command.run(env={}, aslr=True)
+            return command
+
+    @dataclass
+    class Attacher(__GdbOpener, Attacher):
+        gdbserver: str = 'gdbserver'
+
+        def attach(self, pid: int) -> list[str]:
+            return [self.gdbserver, '--attach', f'{self.host}:{self.port}', f'{pid}']
+
+
 @dataclass
-class GdbDebugger(GdbOpener, Debugger):
-    options: list[str] = field(default_factory=list)
-    gdbserver: str = 'gdbserver'
-    env: str = 'env'
-
-    def debug(self, *, env: dict[str, str] = {}, aslr: bool = True) -> list[str]:
-        command = [self.gdbserver]
-
-        if aslr:
-            command += ['--no-disable-randomization']
-        else:
-            command += ['--disable-randomization']
-
-        if env:
-            command += ['--wrapper', self.env]
-
-            for k, v in env.items():
-                command += [f'{k}={v}']
-
-            command += ['--']
-
-        command += self.options
-        command += [f'{self.host}:{self.port}']
-        command += self.command.run(env={}, aslr=True)
-        return command
-
-
-@dataclass
-class GdbAttacher(GdbOpener, Attacher):
-    gdbserver: str = 'gdbserver'
-
-    def attach(self, pid: int) -> list[str]:
-        return [self.gdbserver, '--attach', f'{self.host}:{self.port}', f'{pid}']
-
-
-@dataclass
-class RR(GdbOpener, ReverseDebugger):
+class RR(__GdbOpener, ReverseDebugger):
     options: list[str] = field(default_factory=list)
     rr: str = 'rr'
     setarch: str = 'setarch'
